6b7417b7fc8ef4d275eee7f7677cac82dcfea7df
Ben Houston <BenjaminAHouston@gmail.com>
2019-01-25 01:46:56 -0500

    More refactoring

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
 }
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks");
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks');
 
 app.listen(PORT, () => {
     console.log(`App listening on port: ${PORT}`);
